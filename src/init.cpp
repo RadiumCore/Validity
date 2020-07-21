2df0bb2db0b4ba5732d3e71aee70a34f4ec3f895
Justin <Justinj100@gmail.com>
2020-07-21 00:19:18 -0400

    Revert "Force tx rescan on startup"
    This reverts commit d9b6e907a9f706c2d53cbcf3f29e130c175e3419.

--- src/init.cpp
+++ src/init.cpp
@@ -756,7 +756,7 @@ void InitParameterInteraction()
     }
 
     // -zapwallettx implies a rescan
-    if (GetBoolArg("-zapwallettxes", true)) {
+    if (GetBoolArg("-zapwallettxes", false)) {
         if (SoftSetBoolArg("-rescan", true))
             LogPrintf("%s: parameter interaction: -zapwallettxes=<mode> -> setting -rescan=1\n", __func__);
     }
