a0ae896017e3b1661683e872cc82968b48605ab7
Michel van Kessel <michelvankessel@protonmail.com>
2021-04-04 11:49:00 +0200

    cross-compile update dependencies

--- depends/packages/bdb.mk
+++ depends/packages/bdb.mk
@@ -6,19 +6,22 @@ $(package)_sha256_hash=99ccd944ffcccc88c0f404b4f3d8cb10747e1e3dfe9ec566f518725f9
 $(package)_build_subdir=build_unix
 
 define $(package)_set_vars
-$(package)_config_opts=--disable-shared --enable-cxx --disable-replication
+$(package)_config_opts=--disable-shared --enable-cxx --disable-replication --enable-option-checking
 $(package)_config_opts_mingw32=--enable-mingw
 $(package)_config_opts_mingw64=--enable-mingw
 $(package)_config_opts_linux=--with-pic
+$(package)_config_opts_freebsd=--with-pic
+ifneq ($(build_os),darwin)
+$(package)_config_opts_darwin=--disable-atomicsupport
+endif
+$(package)_config_opts_aarch64=--disable-atomicsupport
 $(package)_cxxflags=-std=c++11
 $(package)_cppflags_mingw32=-DUNICODE -D_UNICODE
 endef
 
 define $(package)_preprocess_cmds
   sed -i.old 's/WinIoCtl.h/winioctl.h/g' src/dbinc/win_db.h && \
-  sed -i.old 's/__atomic_compare_exchange\\(/__atomic_compare_exchange_db(/' src/dbinc/atomic.h && \
-  sed -i.old 's/atomic_init/atomic_init_db/' src/dbinc/atomic.h src/mp/mp_region.c src/mp/mp_mvcc.c src/mp/mp_fget.c src/mutex/mut_method.c src/mutex/mut_tas.c && \
-  cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub dist
+  sed -i.old 's/atomic_init/atomic_init_db/' src/dbinc/atomic.h src/mp/mp_region.c src/mp/mp_mvcc.c src/mp/mp_fget.c src/mutex/mut_method.c src/mutex/mut_tas.c
 endef
 
 define $(package)_config_cmds
@@ -32,4 +35,3 @@ endef
 define $(package)_stage_cmds
   $(MAKE) DESTDIR=$($(package)_staging_dir) install_lib install_include
 endef
-
